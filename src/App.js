167fc897b5bd646d5554997913604176babc3d3a
wavfox <tylerjbroussard@wavfox.com>
2021-02-27 12:50:43 -0800

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <h1>Hello!</h1>
+        <p>Let's get started!!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
